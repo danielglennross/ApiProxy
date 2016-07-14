b340ca82e00bef297c6679e712d6a34c7fb80bdc
danielglennross <danielglennross@gmail.com>
2016-07-14 09:39:17 +0100

    update async helper

--- src/utils/async.js
+++ src/utils/async.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports = (makeGenerator) => {
-    return () => {
-        const generator = makeGenerator.apply(this, arguments);
+    // dont use fat arrow, as we dont want 'this' lexically bound
+    return function () {
+        var generator = makeGenerator.apply(this, arguments);
 
-        handle = (result) => {
+        function handle(result){
             // result => { done: [Boolean], value: [Object] }
             if (result.done) return Promise.resolve(result.value);
 
@@ -13,43 +14,12 @@ module.exports = (makeGenerator) => {
             }, err => {
                 return handle(generator.throw(err));
             });
-        };
+        }
 
         try {
             return handle(generator.next());
         } catch (ex) {
             return Promise.reject(ex);
         }
-    }
-};
-
-
-
-
-
-
-
-
-
-module.exports = (makeGenerator) => {
-    return function () {
-    var generator = makeGenerator.apply(this, arguments);
-
-    function handle(result){
-      // result => { done: [Boolean], value: [Object] }
-      if (result.done) return Promise.resolve(result.value);
-
-      return Promise.resolve(result.value).then(function (res){
-        return handle(generator.next(res));
-      }, function (err){
-        return handle(generator.throw(err));
-      });
-    }
-
-    try {
-      return handle(generator.next());
-    } catch (ex) {
-      return Promise.reject(ex);
-    }
-  }
+    };
 };
